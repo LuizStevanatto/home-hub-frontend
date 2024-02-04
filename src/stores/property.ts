142fb34f4c2d5c1d0041a6a56085a183dee672a1
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2024-02-04 16:25:26 -0300

    add property routes in store

---
+++ src/stores/property.ts
@@ -0,0 +1,43 @@
+import api from "@/services/api";
+import { create } from "zustand";
+
+export interface IProperty {
+  id: string;
+	name: string;
+	address: string;
+	city: string;
+	state: string;
+	zipCode: string;
+	country: string;
+	description: string;
+	price: number;
+	isAvailable: boolean;
+	ownerId: string;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+interface IUsePropertyStore {
+  createProperty: (property: IProperty) => void
+  getProperty: (id: string) => void
+  updateProperty: (property: IProperty) => void
+  deleteProperty: (id: string) => void
+}
+
+export const usePropertyStore = create<IUsePropertyStore>((set) => ({
+  async createProperty(data: IProperty) {
+    await api.post('/property', data)
+  },
+
+  async getProperty(id: string) {
+    return await api.get(`property/${id}`)
+  },
+
+  async updateProperty(data: IProperty) {
+    await api.put(`property/${data.id}`, data)
+  },
+
+  async deleteProperty(id: string) {
+    await api.delete(`property/${id}`)
+  },
+}))
