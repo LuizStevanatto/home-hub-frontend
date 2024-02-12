3875f43126fa40dc7fe752d811147c759dd374d1
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2024-02-12 16:18:16 -0300

    update width in layout root

--- src/layout/root.tsx
+++ src/layout/root.tsx
@@ -8,7 +8,7 @@ export function LayoutRoot(props: LayoutRootProps) {
   const { children } = props;
 
   return (
-    <main className="w-screen">
+    <main className="max-w-screen">
       <Header />
       <div className="h-[calc(100vh-197px)]">{children}</div>
       <Footer />
