c0c7a3790f08bc3bdfe22ec8ca537151806f0918
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2024-03-07 12:06:22 -0300

    update style property/id page

--- src/pages/property/[id]/index.tsx
+++ src/pages/property/[id]/index.tsx
@@ -7,7 +7,6 @@ import Container from "@/components/Conateiner";
 import { IProperty, usePropertyStore } from "@/stores/property";
 import { LayoutRoot } from "@/layout/root";
 import { toast } from "react-toastify";
-
 export default function Property() {
   const { getProperty, deleteProperty } = usePropertyStore();
   const [property, setProperty] = useState<IProperty | null>(null);
@@ -67,26 +66,15 @@ export default function Property() {
       </Head>
 
       <Container className="mt-12">
-        <main className="flex flex-col sm:flex-row sm:items-center sm:gap-5">
-          <div className="h-max w-full sm:min-w-min sm:max-w-max p-7 bg-white border border-gray5 rounded-b-lg flex flex-col sm:relative sm:z-10 sm:-mt-10 sm:p-9 sm:flex-col sm:gap-5 sm:rounded-lg">
-            <div className="font-medium flex items-center gap-4">
-              <p className="text-gray1">{property?.name}</p>
-            </div>
+        <main className="flex flex-col  sm:items-center sm:gap-5 border-2 border-gray-300 rounded-[8px] p-4 max-w-[580px]">
 
-            <div className="my-7 h-[0.0625rem] w-full bg-gray5" />
-
-            <div className="text-2xl font-semibold flex items-center sm:justify-center">
-              <p className="text-brand1">{priceFormatted}</p>
-            </div>
-          </div>
-
-          <div className="w-full mt-2 flex flex-col gap-9 max-w-3xl">
+          <div className="w-full mt-2 flex flex-col gap-4 max-w-3xl">
             <section className="max-w-xl">
               <h2 className="text-2xl text-gray1 font-semibold leading-normal">
                 {property?.name}
               </h2>
               <div className="mt-4 text-gray2 flex items-center gap-1">
-                <IoLocationOutline />
+                <IoLocationOutline/>
                 <p>
                   {property?.address}, {property?.city} - {property?.state}
                 </p>
@@ -94,45 +82,49 @@ export default function Property() {
             </section>
 
             <section className="text-gray1 leading-normal">
-              <h3 className="text-xl  font-semibold ">Descrição do imóvel</h3>
+              <h3 className="text-xl text-brand1 font-semibold ">Descrição do imóvel</h3>
               <p className="mt-5">{property?.description}</p>
             </section>
 
             <section className="text-gray1 leading-normal flex flex-col gap-3">
-              <h3 className="text-xl  font-semibold ">Detalhes do imóvel</h3>
-              <span>Á venda: {property?.isAvailable ? "Sim" : "Não"}</span>
+              <h3 className="text-xl text-brand1 font-semibold ">Detalhes do imóvel</h3>
+
+              <p>Valor: <strong>{priceFormatted}</strong></p>
+              <span>Á venda: <strong>{property?.isAvailable ? "Não" : "Sim"}</strong></span>
             </section>
           </div>
+
+          <div className="flex flex-col sm:flex-row sm:justify-start sm:gap-2">
+            <button
+                className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
+                onClick={() => {
+                  router.push(`/property/${propertyId}/edit-property`);
+                }}
+            >
+              Editar propriedade
+            </button>
+
+            {!property?.isAvailable && (
+                <button
+                    className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
+                    onClick={() => {
+                      router.push(`/property/${propertyId}/new-contract`);
+                    }}
+                >
+                  Novo Contrato
+                </button>
+            )}
+
+            <button
+                className="bg-red-500 hover:bg-red-600 p-4 rounded-lg text-white font-semibold mt-8"
+                onClick={handleDeleteProperty}
+            >
+              Deletar Contrato
+            </button>
+          </div>
+
         </main>
 
-        <div className="flex flex-col sm:flex-row sm:items-center sm:gap-2">
-          <button
-            className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
-            onClick={() => {
-              router.push(`/property/${propertyId}/edit-property`);
-            }}
-          >
-            Editar propriedade
-          </button>
-
-          {!property?.isAvailable && (
-            <button
-              className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
-              onClick={() => {
-                router.push(`/property/${propertyId}/new-contract`);
-              }}
-            >
-              Novo Contrato
-            </button>
-          )}
-
-          <button
-            className="bg-red-500 hover:bg-red-600 p-4 rounded-lg text-white font-semibold mt-8"
-            onClick={handleDeleteProperty}
-          >
-            Deletar Contrato
-          </button>
-        </div>
       </Container>
     </>
   );
