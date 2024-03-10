fbaa8bc0bab726f50018d00bdef70d5f3b4d8af9
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2024-03-10 00:22:29 -0300

    update property-id box style

--- src/pages/property/[id]/index.tsx
+++ src/pages/property/[id]/index.tsx
@@ -66,15 +66,14 @@ export default function Property() {
       </Head>
 
       <Container className="mt-12">
-        <main className="flex flex-col  sm:items-center sm:gap-5 border-2 border-gray-300 rounded-[8px] p-4 max-w-[580px]">
-
+        <main className="flex flex-col  sm:items-center sm:gap-5 bg-neutral-300 rounded-[8px] p-4 max-w-[580px]">
           <div className="w-full mt-2 flex flex-col gap-4 max-w-3xl">
             <section className="max-w-xl">
               <h2 className="text-2xl text-gray1 font-semibold leading-normal">
                 {property?.name}
               </h2>
-              <div className="mt-4 text-gray2 flex items-center gap-1">
-                <IoLocationOutline/>
+              <div className="mt-4 text-black flex items-center gap-1">
+                <IoLocationOutline />
                 <p>
                   {property?.address}, {property?.city} - {property?.state}
                 </p>
@@ -82,49 +81,56 @@ export default function Property() {
             </section>
 
             <section className="text-gray1 leading-normal">
-              <h3 className="text-xl text-brand1 font-semibold ">Descrição do imóvel</h3>
+              <h3 className="text-xl text-brand1 font-semibold ">
+                Descrição do imóvel
+              </h3>
               <p className="mt-5">{property?.description}</p>
             </section>
 
             <section className="text-gray1 leading-normal flex flex-col gap-3">
-              <h3 className="text-xl text-brand1 font-semibold ">Detalhes do imóvel</h3>
+              <h3 className="text-xl text-brand1 font-semibold ">
+                Detalhes do imóvel
+              </h3>
 
-              <p>Valor: <strong>{priceFormatted}</strong></p>
-              <span>Á venda: <strong>{property?.isAvailable ? "Sim" : "Não"}</strong></span>
+              <p>
+                Valor: <strong>{priceFormatted}</strong>
+              </p>
+              <span>
+                Á venda:{" "}
+                <strong>{property?.isAvailable ? "Sim" : "Não"}</strong>
+              </span>
             </section>
           </div>
 
           <div className="flex flex-col sm:flex-row sm:justify-start sm:gap-2">
             <button
-                className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
-                onClick={() => {
-                  router.push(`/property/${propertyId}/edit-property`);
-                }}
+              className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
+              onClick={() => {
+                router.push(`/property/${propertyId}/edit-property`);
+              }}
             >
               Editar propriedade
             </button>
 
             {property?.isAvailable && (
-                <button
-                    className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
-                    onClick={() => {
-                      router.push(`/property/${propertyId}/new-contract`);
-                    }}
-                >
-                  Novo Contrato
-                </button>
+              <button
+                className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
+                onClick={() => {
+                  router.push(`/property/${propertyId}/new-contract`);
+                }}
+              >
+                Novo Contrato
+              </button>
             )}
 
             <button
-                className="bg-red-500 hover:bg-red-600 p-4 rounded-lg text-white font-semibold mt-8"
-                onClick={handleDeleteProperty}
+              className="bg-red-500 hover:bg-red-600 p-4 rounded-lg text-white font-semibold mt-8"
+              onClick={handleDeleteProperty}
             >
               Deletar Contrato
             </button>
           </div>
-
         </main>
-
       </Container>
     </>
   );
