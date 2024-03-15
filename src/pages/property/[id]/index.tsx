4b7337578d87492caaa36799bcf1436074e897e6
lucasfelipedonascimento <lucasfelipe3000@gmail.com>
2024-03-14 23:22:28 -0300

    add new section on property screen

--- src/pages/property/[id]/index.tsx
+++ src/pages/property/[id]/index.tsx
@@ -7,18 +7,26 @@ import Container from "@/components/Conateiner";
 import { IProperty, usePropertyStore } from "@/stores/property";
 import { LayoutRoot } from "@/layout/root";
 import useUserStore from "@/stores/user";
+import { IContract, useContractsStore } from "@/stores/contracts";
+import { dateFormat } from "@/utils/date-format";
 export default function Property() {
   const { user } = useUserStore();
   const { getProperty } = usePropertyStore();
+  const { getPropertyContracts } = useContractsStore();
   const [property, setProperty] = useState<IProperty | null>(null);
+  const [propertyContracts, setPropertyContracts] = useState<
+    IContract[] | null
+  >(null);
   const router = useRouter();
   const propertyId = String(router.query.id);
 
   const getPropertyInDb = React.useCallback(async () => {
-    const getPropertyById = await getProperty(String(propertyId));
+    const getPropertyById = await getProperty(propertyId);
+    const getContractsPropertyById = await getPropertyContracts(propertyId);
 
     setProperty(getPropertyById);
-  }, [getProperty, propertyId]);
+    setPropertyContracts(getContractsPropertyById);
+  }, [getProperty, getPropertyContracts, propertyId]);
 
   React.useEffect(() => {
     getPropertyInDb();
@@ -41,8 +49,8 @@ export default function Property() {
         <title>Propriedade</title>
       </Head>
 
-      <Container className="mt-12">
-        <main className="flex flex-col  sm:items-center sm:gap-5 bg-neutral-300 rounded-[8px] p-4 max-w-[580px]">
+      <Container className="mt-12 flex flex-col sm:flex-row gap-6">
+        <main className="flex flex-col sm:w-[420px] sm:items-center sm:gap-5 bg-neutral-300 rounded-[8px] p-4 space-y-6 sm:space-y-0">
           <div className="w-full mt-2 flex flex-col gap-4 max-w-3xl">
             <section className="max-w-xl">
               <h2 className="text-2xl text-gray1 font-semibold leading-normal">
@@ -94,32 +102,70 @@ export default function Property() {
             </section>
           </div>
 
-          <div className="flex flex-col sm:flex-row sm:justify-start sm:gap-2">
-            {user?.id === property?.ownerId && (
-              <>
+          <div className="flex flex-col sm:justify-start sm:gap-2">
+            <div className="flex flex-col sm:flex-row gap-2 sm:gap-5">
+              {user?.id === property?.ownerId && (
+                <>
+                  <button
+                    className="bg-brand2 hover:bg-brand1 p-4 rounded-lg text-white font-semibold mt-8"
+                    onClick={() => {
+                      router.push(`/property/${propertyId}/edit-property`);
+                    }}
+                  >
+                    Editar propriedade
+                  </button>
+                </>
+              )}
+
+              {property?.isAvailable && (
                 <button
-                  className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
+                  className="bg-green-600 hover:bg-green-700 p-4 rounded-lg text-white font-semibold mt-8"
                   onClick={() => {
-                    router.push(`/property/${propertyId}/edit-property`);
+                    router.push(`/property/${propertyId}/new-contract`);
                   }}
                 >
-                  Editar propriedade
+                  Novo Contrato
                 </button>
-              </>
-            )}
-
-            {user?.id !== property?.ownerId && property?.isAvailable && (
-              <button
-                className="bg-brand2 p-4 rounded-lg text-white font-semibold mt-8"
-                onClick={() => {
-                  router.push(`/property/${propertyId}/new-contract`);
-                }}
-              >
-                Novo Contrato
-              </button>
-            )}
+              )}
+            </div>
           </div>
         </main>
+
+        {propertyContracts && propertyContracts.length > 0 && (
+          <>
+            <div className="flex flex-col gap-2 p-4 bg-neutral-300 w-full sm:w-[420px] rounded-[8px] space-y-4">
+              <h3 className="text-xl mt-2  text-brand1 font-semibold ">
+                Outros contratos dessa propriedade
+              </h3>
+
+              {propertyContracts.map((item) => {
+                return (
+                  <div key={item.id} className="flex flex-col">
+                    <span>
+                      Data de início:{" "}
+                      <strong>{dateFormat(item.startDate)}</strong>
+                    </span>
+                    <span>
+                      Data de Término:{" "}
+                      <strong>{dateFormat(item.endDate)}</strong>
+                    </span>
+                    <span>
+                      Valor:{" "}
+                      <strong>{handleFormattedCurrency(item.price)}</strong>
+                    </span>
+
+                    <span>
+                      Situação:{" "}
+                      <strong>{item.isActive ? "Ativo" : "Encerrado"}</strong>
+                    </span>
+
+                    <div className="border border-gray-400 mt-3" />
+                  </div>
+                );
+              })}
+            </div>
+          </>
+        )}
       </Container>
     </>
   );
--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -1,5 +1,4 @@
 export const dateFormat = (date: string) => {
-    console.log('date', date)
     const formattedDate = new Intl.DateTimeFormat('pt-BR', {
         dateStyle: 'short',
         timeZone: 'America/Sao_Paulo'
